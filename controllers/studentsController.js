950a83957dcfbb0e8665b7714ced4713bab72478
Sang Lee <sanglee.dev@gmail.com>
2018-04-01 20:16:28 -0700

    added contorllers

--- controllers/studentsController.js
+++ controllers/studentsController.js
@@ -1,34 +1,34 @@
 const db = require("../models");
 
-// Defining methods for the booksController
+// Defining methods for the StudentsController
 module.exports = {
   findAll: function(req, res) {
-    db.Book
+    db.Student
       .find(req.query)
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.Book
+    db.Student
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.Book
+    db.Student
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Book
+    db.Student
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.Book
+    db.Student
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
