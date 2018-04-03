ae9350f47f709577e2de9b01d16390e18c043256
Jun-Wei Chen <jwchen1259@gmail.com>
2018-04-03 06:04:24 -0700

    Fix landing page's layout & add login page

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Background from './homebg.jpg';
 import videojs from 'video.js';
 import 'video.js/dist/video-js.css';
 import './Landing.css';
@@ -12,6 +11,7 @@ class Landing extends Component {
         autoplay: true,
         loop: true,
         controls: false,
+        muted: true,
         sources: [{
             src: '../bg.mp4',
             type: 'video/mp4'
@@ -30,31 +30,21 @@ class Landing extends Component {
   render() {
     return (
         <div className="site-wrapper">
-          <div className="video-background-container" div-vjs-player>
+          <div className="video-container" div-vjs-player>
             <video ref={node => this.videoNode = node} className="video-js"></video>
-            <div className="overlay"/>
             <div className="site-wrapper-inner">
               <div className="masthead clearfix">
                 <div className="inner">
-                  <h3 className="masthead-brand"><a href="#home"><img src="images/ableft.png"/><br /></a></h3>
-                  <nav>
-                    <form id="signin" className="navbar-form navbar-right" role="form">
-                      <div className="input-group">
-                        <input id="email" type="email" className="form-control" name="email" defaultValue placeholder="Email Address" />                                        
-                      </div>
-                      <div className="input-group">
-                        <input id="password" type="password" className="form-control" name="password" defaultValue placeholder="Password" />                                        
-                      </div>
-                      <button type="submit" className="btn btn-warning">Login</button>
-                    </form>
-                  </nav>
+                  <div className="masthead-brand">
+                    <a href="/"><img src="images/ableft.png" /><br /></a>
+                  </div>
                 </div>
               </div>
               <div className="cover-container">
                 <div className="inner cover">
                   <h1 className="cover-heading">Teachaid</h1>
                   <p className="lead">This application will provide conflict resolution for nominating students of the month.</p>
-                  <p className="lead"><a href="login.html" className="btn btn-lg btn-warning">REGISTER</a></p>
+                  <p className="lead"><a href="/login" className="btn btn-lg btn-warning">REGISTER</a></p>
                 </div>
                 <div className="mastfoot">
                   <div className="inner">
