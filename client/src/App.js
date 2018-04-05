8e9be76da70880d2251a4b029048a27e5322c72a
pdlc89 <pcruz9332@gmail.com>
2018-04-05 15:33:35 -0700

    should work

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,10 @@ import Login from "./pages/Login";
 import Students from "./pages/Students";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+//import Splash from "./pages/Splash";
+import Nominated from "./pages/Nominated";
+import History from "./pages/History";
 
 const App = () => (
   <Router>
@@ -12,6 +16,10 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route exact path="/login" component={Login} />
+        {/*<Route exact path="/" component={Splash}/>*/}
+        <Route exact path="/nominated" component={Nominated} />
+        <Route exact path="/nominate" component={Students} />
+        <Route exact path="/history" component={History} />
         <Route exact path="/students/:id" component={Detail} />
         <Route component={NoMatch} />
       </Switch>
