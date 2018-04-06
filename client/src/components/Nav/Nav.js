1dcac50d013fda624b6c1a2bc318f590d86204d6
Jun-Wei Chen <jwchen1259@gmail.com>
2018-04-05 19:40:11 -0700

    Fix links on navbar

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -5,18 +5,18 @@ const Nav = () => (
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
                 <li className="nav-item active">
-                    <a className="nav-link" href="./students.html">Home <span className="sr-only">(current)</span></a>
+                    <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="./nominate.html">Nominate</a>
+                    <a className="nav-link" href="/nominate">Nominate</a>
                 </li>
                 <li className="nav-item dropdown">
                     <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Students
                     </a>
                     <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a className="dropdown-item" href="#">Current Month</a>
-                        <a className="dropdown-item" href="#">History</a>
+                        <a className="dropdown-item" href="/nominated">Current Month</a>
+                        <a className="dropdown-item" href="/history">History</a>
                         <div className="dropdown-divider" />
                         <a className="dropdown-item" href="#">Donate</a>
                     </div>
